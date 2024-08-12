d6d9d8c34cbce41e1bb3c4be335cac4639638513
FrexCheat <frexcheat@163.com>
2024-08-12 23:14:00 +0800

    feature: add inner events update api and labs api.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,37 @@
-import { Context, Schema } from 'koishi'
+import { Context, Schema, h } from 'koishi'
 import { Events } from './service/events'
 import { InnerEvents } from './service/events_in'
-import { InnerEvent } from './types'
+import { Labs } from './service/labs'
+import { InnerEvent, Lab } from './types'
 
 export const name = 'devb-doc-api'
 
-export const inject = ['http']
+export const inject = ['http', 'database']
+
+interface Group {
+  platform: string,
+  uid: string
+}
 
 export interface Config {
   baseURL: string,
   checker: string,
+  groups: Group[]
 }
 
 export const Config: Schema<Config> = Schema.object({
   baseURL: Schema.string().required().description('API请求地址'),
-  checker: Schema.string().description('审核者账号')
+  checker: Schema.string().description('接收审核信息的账号'),
+  groups: Schema.array(Schema.object({
+    platform: Schema.string().required(),
+    uid: Schema.string().required()
+  })).description('接收审核信息的群组').role('table')
 })
 
 export function apply(ctx: Context, config: Config) {
   let events = new Events(ctx, config)
   let inner = new InnerEvents(ctx, config)
+  let labs = new Labs(ctx, config)
 
   ctx.command('devb-e [plat:text]', '查询校外比赛信息').action(async (_, plat) => {
     let res = await events.getEvents(plat)
@@ -31,7 +43,7 @@ export function apply(ctx: Context, config: Config) {
     return res
   })
 
-  ctx.command('devb-unine', '查询未审核比赛信息').action(async () => {
+  ctx.command('devb-unine', '查询未审核的校内比赛信息').action(async () => {
     let res = await inner.getEvents(false)
     return res
   })
@@ -41,7 +53,7 @@ export function apply(ctx: Context, config: Config) {
     .option('summary', '-s [比赛简介]', { fallback: '暂无' })
     .option('notes', '-e [比赛说明]', { fallback: '暂无' })
     .option('time', '-t [比赛时间]', { fallback: '暂无' })
-    .action(async ({ options }) => {
+    .action(async ({ session, options }) => {
       let data: InnerEvent = {
         uid: null,
         name: options.name,
@@ -51,13 +63,72 @@ export function apply(ctx: Context, config: Config) {
         release: false
       }
       let res = await inner.uploadEvent(data)
-      return res
+      session.send(h('message', h.quote(session.messageId), res))
     })
 
-  ctx.command('devb-deine', '删除比赛信息')
+  ctx.command('devb-updine <uid:string> <column:string> <content:string>', '更新校内比赛信息')
+    .action(async ({ session }, uid, column, content) => {
+      if (uid === undefined || column === undefined || content === undefined) {
+        session.send(h('message', h.quote(session.messageId), '参数不足，请补充参数!'))
+      }
+      let res = await inner.updateEvent(uid, column, content)
+      session.send(h('message', h.quote(session.messageId), res))
+    })
+
+  ctx.command('devb-deine', '删除校内比赛信息')
     .option('uid', '-u <比赛UID>')
-    .action(async ({ options }) => {
+    .action(async ({ session, options }) => {
       let res = await inner.deleteEvent(options.uid)
-      return res
+      session.send(h('message', h.quote(session.messageId), res))
+    })
+
+  ctx.command('devb-lab', '查询校内实验室信息').action(async () => {
+    let res = await labs.getLabs(true)
+    return res
+  })
+
+  ctx.command('devb-unlab', '查询未审核实验室信息').action(async () => {
+    let res = await labs.getLabs(false)
+    return res
+  })
+
+  ctx.command('devb-uplab', '上传校内实验室信息')
+    .option('name', '-n <实验室名称>')
+    .option('summary', '-s [实验室简介]', { fallback: '暂无' })
+    .option('college', '-c [实验室学院]', { fallback: '待上传' })
+    .option('position', '-p [实验室位置]', { fallback: '待上传' })
+    .option('limit', '-l [招新范围]', { fallback: '暂无' })
+    .option('group', '-g [招新交流群]', { fallback: '暂无' })
+    .option('time', '-t [招新时间]', { fallback: '暂无' })
+    .action(async ({ session, options }) => {
+      let data: Lab = {
+        uid: null,
+        name: options.name,
+        summary: options.summary,
+        college: options.college,
+        position: options.position,
+        limit: options.limit,
+        group: options.group,
+        time: options.time,
+        release: false
+      }
+      let res = await labs.uploadLab(data)
+      session.send(h('message', h.quote(session.messageId), res))
+    })
+
+  ctx.command('devb-updlab <uid:string> <column:string> <content:string>', '更新实验室信息')
+    .action(async ({ session }, uid, column, content) => {
+      if (uid === undefined || column === undefined || content === undefined) {
+        session.send(h('message', h.quote(session.messageId), '参数不足，请补充参数!'))
+      }
+      let res = await labs.updateLab(uid, column, content)
+      session.send(h('message', h.quote(session.messageId), res))
+    })
+
+  ctx.command('devb-delab', '删除实验室信息')
+    .option('uid', '-u <实验室UID>')
+    .action(async ({ session, options }) => {
+      let res = await labs.deleteLab(options.uid)
+      session.send(h('message', h.quote(session.messageId), res))
     })
 }
--- src/service/labs.ts
+++ src/service/labs.ts
@@ -1,62 +1,64 @@
-import { InnerEvent, innerCol } from '../types'
+import { Lab, labCol } from '../types'
 import { Context } from 'koishi'
 import { Config } from '..'
 
-export class InnerEvents {
+export class Labs {
     constructor(private ctx: Context, private config: Config) { }
 
-    async getEvents(release: boolean) {
-        let apiURL = release ? '/api/event/inner/get?release=true' : '/api/event/inner/get?release=false'
+    async getLabs(release: boolean) {
+        let apiURL = release ? '/api/lab/get?release=true' : '/api/lab/get?release=false'
         let temp = await this.ctx.http.get(this.config.baseURL + apiURL)
         if ('status' in temp) {
             if (temp['status'] === 'error') {
                 return `获取失败! ${temp['message']}`
             }
         }
-        let data: InnerEvent[] = temp
-        let res: string = `共找到 ${data.length} 场校内比赛：\n`
+        let data: Lab[] = temp
+        let res: string = `共找到 ${data.length} 个校内实验室：\n`
         data.forEach((obj) => {
             res = res.concat('------------------\n')
-            res = res.concat(`比赛UID：${obj.uid}\n`)
-            res = res.concat(`比赛名称：${obj.name}\n`)
-            res = res.concat(`比赛简介：${obj.summary}\n`)
-            res = res.concat(`比赛时间：${obj.startAt}\n`)
+            res = res.concat(`实验室UID：${obj.uid}\n`)
+            res = res.concat(`实验室名称：${obj.name}\n`)
+            res = res.concat(`实验室简介：${obj.summary}\n`)
         })
         res = res.concat('------------------\n')
-        res = res.concat('比赛详情: https://newbie.frexlink.cn/main.html')
+        res = res.concat('信息详情: https://newbie.frexlink.cn/main.html')
         return res
     }
 
-    async uploadEvent(data: InnerEvent) {
+    async uploadLab(data: Lab) {
         if (data.name === undefined || data.name.length < 5) {
-            return '比赛名称格式错误或不存在!'
+            return '实验室名称格式错误或不存在!'
         }
-        let resp = await this.ctx.http.post(this.config.baseURL + `/api/event/inner/create`, data)
+        let resp = await this.ctx.http.post(this.config.baseURL + `/api/lab/create`, data)
         if ('status' in resp) {
             if (resp['status'] === 'error') {
                 return `上传失败! ${resp['message']}`
             }
         }
-        let msg: string = `有新校内比赛待审核：\n`
+        let msg: string = `有新校内实验室信息待审核：\n`
         msg = msg.concat('------------------\n')
-        msg = msg.concat(`比赛名称：${data.name}\n`)
-        msg = msg.concat(`比赛简介：${data.summary}\n`)
-        msg = msg.concat(`比赛说明：${data.notes}\n`)
-        msg = msg.concat(`比赛时间：${data.startAt}\n`)
+        msg = msg.concat(`实验室名称${data.name}\n`)
+        msg = msg.concat(`实验室简介${data.summary}\n`)
+        msg = msg.concat(`实验室学院${data.college}\n`)
+        msg = msg.concat(`实验室位置${data.position}\n`)
+        msg = msg.concat(`招新范围${data.limit}\n`)
+        msg = msg.concat(`招新交流群${data.group}\n`)
+        msg = msg.concat(`招新时间${data.time}\n`)
         msg = msg.concat('------------------\n')
-        msg = msg.concat('使用 devb-unine 命令查看未审核的校内比赛列表。')
+        msg = msg.concat('使用 devb-unlab 命令查看未审核的校内比赛列表。')
         this.config.groups.forEach(obj => {
             this.ctx.broadcast([`${obj.platform}:${obj.uid}`], msg)
         })
         return '上传成功! 请等待管理员审核。'
     }
 
-    async updateEvent(uid: string, column: string, cont: any) {
+    async updateLab(uid: string, column: string, cont: any) {
         if (uid === undefined) {
-            return '比赛UID不能为空!'
+            return '实验室UID不能为空!'
         }
         let data = { content: cont }
-        if (innerCol.includes(column)) {
+        if (labCol.includes(column)) {
             if (column === 'release') {
                 if (cont != 'true' && cont != 'false') {
                     return '字段值错误! 请输入 true 或 false'
@@ -65,7 +67,7 @@ export class InnerEvents {
                     cont === 'true' ? data = { content: true } : data = { content: false }
                 }
             }
-            let resp = await this.ctx.http.post(this.config.baseURL + `/api/event/inner/update?uid=${uid}&column=${column}`, data)
+            let resp = await this.ctx.http.post(this.config.baseURL + `/api/lab/update?uid=${uid}&column=${column}`, data)
             if ('status' in resp) {
                 if (resp['status'] === 'error') {
                     return `修改失败! ${resp['message']}`
@@ -78,11 +80,11 @@ export class InnerEvents {
         }
     }
 
-    async deleteEvent(uid: string) {
+    async deleteLab(uid: string) {
         if (uid === undefined) {
-            return '比赛UID不能为空!'
+            return '实验室UID不能为空!'
         }
-        let resp = await this.ctx.http.post(this.config.baseURL + `/api/event/inner/delete?uid=${uid}`)
+        let resp = await this.ctx.http.post(this.config.baseURL + `/api/lab/delete?uid=${uid}`)
         if ('status' in resp) {
             if (resp['status'] === 'error') {
                 return `删除失败! ${resp['message']}`
