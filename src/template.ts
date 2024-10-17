bad35cf89b86f3c77ace67d72aa2065186765c73
FrexCheat <admin@frexlink.com>
2024-10-17 17:25:25 +0800

    fix: convert fixed.

--- src/template.ts
+++ src/template.ts
@@ -91,9 +91,9 @@ export async function generateProblemImage(ctx: Context, id: number) {
 
 export async function generateProblemSearchImage(ctx: Context, title: string) {
   const page = await ctx.puppeteer.page()
-  const specialChars = /[!@#$%^&*(),.?":{}|<>]/g
-  title.replace(specialChars, char => encodeURIComponent(char))
-  await page.goto(`http://acm.zzuli.edu.cn/problemset.php?search=${title}`)
+  const specialChars = /[!@#$%^&*(),.?":{}|<>+]/g
+  const convert = title.replace(specialChars, char => encodeURIComponent(char))
+  await page.goto(`http://acm.zzuli.edu.cn/problemset.php?search=${convert}`)
   const list = await page.$('#problemset')
   const screenshot = await list.screenshot({})
   page.close()
